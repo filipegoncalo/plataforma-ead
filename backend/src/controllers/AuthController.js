76c4021dd473d2e07048c9e135ff35f73c507471
filipegoncalo <filipe.gsantos13@gmail.com>
2020-06-10 01:29:38 -0300

    finished CRUD user

---
+++ backend/src/controllers/AuthController.js
@@ -0,0 +1,20 @@
+const knex = require('../database');
+const bcrypt = require('bcrypt');
+
+module.exports = {
+    async login(request, response){ 
+        
+    },
+
+    async register(request, response, next){
+        
+    },
+
+    async forget(request, response, next){
+        
+    },
+
+    async reset(request, response, next){
+        
+    },
+}
