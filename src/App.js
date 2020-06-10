d86976e6816fed71ede13d4d42dbcf0ef5ba0e8d
Pedro Henrique Brito <pedrohenriquebrito6@gmail.com>
2020-06-10 16:10:32 -0300

    Organização de arquivos, importação de imagens

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 
-import Header from './components/Header'
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Home/ >
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,7 @@ import React from 'react';
 function Header(){
     return(
         <header>
-            <h1>Início</h1>
-            <h1>Planos</h1>
+            <h1>Início</h1><h1>Planos</h1>            
         </header>
     )
 }
