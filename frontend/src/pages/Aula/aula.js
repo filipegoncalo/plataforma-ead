95e87b670d9a318671ccdb06496da4d538382bb9
Laurent Herman <31941580+LaurentHP@users.noreply.github.com>
2020-06-25 19:57:12 -0300

    Add files via upload
    Correção do bug do conteudo da página não ficando do lado.

--- frontend/src/pages/Aula/aula.js
+++ frontend/src/pages/Aula/aula.js
@@ -1,7 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
 import '../../pages/Aula/aula.css';
 import '../../assets/setup.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Logo from '../../assets/logoBrancoM.png';
 import EmojiEventsIcon from '@material-ui/icons/EmojiEvents';
 import SchoolIcon from '@material-ui/icons/School';
@@ -10,19 +10,27 @@ import PersonIcon from '@material-ui/icons/Person';
 import CancelIcon from '@material-ui/icons/Cancel';
 
 export default class aula extends Component {
+  state = {open: false,};
+  handleButtonClick = () => {
+    this.setState(state => {
+      return {
+        open: !state.open,
+      };
+    });
+  };
   render() {
     return (
-      <div>
-          <nav className="col-md-2 sticky-top bg-color menuLateral ">
+      <div className="menuSalaDeAula row">
+          <nav className="col-md-2  bg-color menuLateral ">
             <nav>
             <br></br>
               <img className="navbar-brand col-sm-12 logo" src={Logo}/>
             </nav>
             <br></br>
-            <div className="sidebar-sticky">             
+            <div className="">             
               <ul className="nav flex-column">               
                 <li className="nav-item">   
-                  <a className="nav-link active" href="#">
+                  <a className="nav-link" href="#">
                     <EmojiEventsIcon style={{ color: 'white'}}/>   Quizz  
                   </a>
                 </li>
@@ -41,23 +49,33 @@ export default class aula extends Component {
                   <PlaylistAddCheckIcon style={{ color: 'white'}}/>   Chamada
                   </a>
                 </li>
-                <li className="nav-item ">
-                <a className="nav-link" href="#">
-                  <PersonIcon style={{ color: 'white'}}/>   Alunos
-                </a>
-                </li>
 
-                <div className="fixed-bottom">
                 <li className="nav-item">
-                  <a className="nav-link " href="#">
-                  <CancelIcon style={{ color: 'white'}}/>   Encerrar Aula
-                  </a>
+                <a className="nav-link dropAluno" href="#"  onClick={this.handleButtonClick}>
+                <PersonIcon style={{ color: 'white'}}/>   Alunos</a>
+                {this.state.open && (
+                <div className="listaAluno">
+                  <ul>
+                    <a className="nav-link">Aluno 1</a>
+                    <a className="nav-link">Aluno 2</a>
+                    <a className="nav-link">Aluno 3</a>
+                  </ul>
+                </div>)}
                 </li>
-                </div>
-              </ul>
+                </ul>
+                </div> 
+              <div className="encerrarAula col-md-10 sidebar-sticky">
+                  <li className="nav-item">
+                    <a className="nav-link " href="#">
+                      <CancelIcon style={{ color: 'white'}}/>   Encerrar Aula
+                    </a>
+                  </li>
             </div>
           </nav>
-        </div>
+          <div className="col">
+            <h1>Código do Jitsi ---------------------------------------------------</h1>
+          </div>        
+      </div>
     );
   }
 }
