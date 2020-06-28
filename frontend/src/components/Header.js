3148af2fcdd4d0c37ebb10fee140ec7265001596
William8CabralPaiva <william8cabral@gmail.com>
2020-06-28 17:38:02 -0300

    Home less

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import logo from '../assets/logo.png';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 import Routes from '../routes'
+import { createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+
 
 function Header(){
     return(
-        <header>
-            <Link to='/'>
-                <h1>Início</h1>
-            </Link>
-            <h1>Planos</h1>          
+        <header className="o-center">
+            <div className="c-header">
+                <div className="c-links">
+                    <Link to="/">
+                        <img src={logo}></img>
+                    </Link>
+                    <Link to='#'>
+                        Início
+                    </Link>
+                    <Link to='#'>
+                        Planos
+                    </Link>
+                </div>
+                <div className="c-botao">
+                    <Button className="o-btn blue"
+                     href="#" variant="contained"  component={Link}>Entrar
+                    </Button>
+                </div>
+            </div>
         </header>
     )
 }
--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import Grid from '@material-ui/core/Grid';
 import './styles.css';
 import '../../assets/setup.css';
 
@@ -13,66 +13,72 @@ import imgAtivo1 from '../../assets/home-ativo-1.png'
 import imgGirls from '../../assets/home-girls.png'
 import imgNetworking from '../../assets/home-networking.png'
 import imgQuiz from '../../assets/home-quiz.png'
+import { Button } from '@material-ui/core';
 
 function Home() {
   return (
 
     <div>
       <Header />
-
       <main>
-        <div>
-          <h3>
-            Aqui é fácil ensinar e aprender!
-             </h3>
-          <p>
-            Comece a usar agora gratuitamente
-             </p>
-
-          <img src={imgGroup1} alt='Grupo de estudo' />
+        <div className="o-background-blue">
+          <Grid className="o-center c-banner o-espaco-padrao o-row">
+              <Grid item xs={6}>
+                <h1 className="o-title-1"> Aqui é fácil<br/> ensinar <br/> e aprender!</h1>
+                <p>Comece a usar agora gratuitamente</p>
+                <Button className="o-btn gray" href="#" variant="contained"  component={Link}>Cadastrar</Button>
+              </Grid>
+              <Grid item xs={6}>
+                <img className="o-img" src={imgGroup1} alt='Grupo de estudo' />
+              </Grid>
+          </Grid>
         </div>
 
-        <div>
-          <h4>
-            Sua aula nunca foi tão divertida!
-             </h4>
-          <p>
-            Um ambiente de aula completo para que o aluno consiga absorver
-            o máximo do aprendizado, utilizando dinâmicas adptadas das salas
-            de aulas presenciais.
-             </p>
-
-          <img src={imgAtivo1} alt='Garota estudando' />
+        <div className="c-ambiente o-center o-espaco-padrao">
+          <Grid className="o-row"> 
+            <Grid item xs={8} className="u-pr-5">
+              <h2 className="o-title-2">Sua aula nunca foi tão divertida!</h2>
+              <p className="pr-15">
+                Um ambiente de aula completo para que o aluno consiga absorver
+                o máximo do aprendizado, utilizando dinâmicas adptadas das salas
+                de aulas presenciais.
+              </p>
+            </Grid>
+            <Grid item xs={4} >
+              <img className="o-img" src={imgAtivo1} alt='Garota estudando' />
+            </Grid>
+          </Grid>
         </div>
 
-        <div>
-          <p>
-            <img src={imgGirls} alt='Garotas estudando' />
 
-            <br />Seus alunos terão a mesma experiência, seja utilizando a plataforma
-               em um computador ou celular.
-             </p>
-        </div>
+        <div className="c-boxes-home o-center o-espaco-padrao o-pt-0">
+          <Grid className="o-row" >
 
-        <div>
-          <p>
-            <img src={imgQuiz} alt='Quiz' />
+            <Grid item xs={4}>
+              <img className="o-img" src={imgGirls} alt='Garotas estudando' />
+              <p>Seus alunos terão a mesma experiência, seja utilizando a plataforma
+                em um computador ou celular.</p>
+            </Grid>
 
-            <br />Engaje os estudantes com desafios e em um sistema gamificado
-               utilizando as ferramentas que oferecemos.
-             </p>
-        </div>
+            <Grid item xs={4} >
+              <img className="o-img" src={imgQuiz} alt='Quiz' />
+              <p>Engaje os estudantes com desafios e em um sistema gamificado
+                utilizando as ferramentas que oferecemos.
+              </p>
+            </Grid>
 
-        <div>
-          <p>
-            <img src={imgNetworking} alt='Jovens se comunicando remotamente' />
+            <Grid item xs={4}>
+              <img className="o-img" src={imgNetworking} alt='Jovens se comunicando remotamente' />
+              <p>Crie grupo de discussões nas aulas on-line para que seus alunos
+                trabalhem em grupo sob sua supervisão.
+              </p>
+            </Grid>
 
-            <br />Crie grupo de discussões nas aulas on-line para que seus alunos
-               trabalhem em grupo sob sua supervisão.
-             </p>
-        </div>
-
-        <Link to='/cadastro'>Cadastrar</Link>
+          </Grid>
+         </div>
+          <div className="o-center o-text-center o-pb-5">
+            <Button className="o-btn blue" href="#" variant="contained"  component={Link}>Cadastrar</Button>
+          </div>
       </main>
     </div>
   );
