cc743851b56419b7596ee8800725421974a8ece4
Laurent Herman <31941580+LaurentHP@users.noreply.github.com>
2020-07-03 15:59:45 -0300

    Att. Routes Dashboard
    Atualização das rotas na Dashboard

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,7 +3,10 @@ import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Cadastro from './pages/Cadastro';
-import Dashboard from './pages/DashboardScreen';
+import Main from './components/Main';
+import Dashboard from '../src/pages/DashboardProfessor/dashboardProfessor';
+import DashboardDisciplinas from '../src/pages/DisciplinasProfessor/disciplinas';
+import DashboardExercicios from '../src/pages/ExerciciosProfessor/exercicios';
 import Aula from './pages/Aula';
 import ProvasProfessor from './pages/ProvasProfessor';
 
@@ -16,11 +19,11 @@ function Routes() {
                 <Route path='/cadastro' component={Cadastro}/>
                 <Route path='/dashboard' component={Dashboard}/>
                 <Route path='/dashboard/aula' component={Aula}/>
+                <Route path='/disciplinas' component={DashboardDisciplinas}/>
+                <Route path='/exercicios' component={DashboardExercicios}/>
                 <Route path="/provas" component={ProvasProfessor} />
                 <Route component={Aula} path='/aula' />
-                {/* <Route component={CriarAtividade} path='/aula/atividade/criar' />
-                <Route component={AbrirAtividade} path='/aula/atividade/abrir' />
-                <Route component={Atividade} path='/aula/atividade' />  */}
+                
                 </Switch>
         </BrowserRouter>
     )
