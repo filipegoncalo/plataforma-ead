8e882ea742d7b72a05292f39be83e1235b94559f
filipegoncalo <filipe.gsantos13@gmail.com>
2020-07-03 08:07:02 -0300

    refactored authentication

--- backend/src/routes/AuthRoutes.js
+++ backend/src/routes/AuthRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const AuthController = require('../controllers/AuthController');
+const { authSignUp, authSignIn } = require('../validators/auth')
 
 const router = express.Router();
 
-router.post('/sign-in', AuthController.login);
+router.post('/sign-in', authSignIn, AuthController.signIn);
 
-router.post('/sign-up',  AuthController.register);
+router.post('/sign-up', authSignUp, AuthController.signUp);
 
 router.post('/reset', AuthController.reset);
 
--- backend/src/validators/auth.js
+++ backend/src/validators/auth.js
@@ -1,25 +1,52 @@
 const Joi = require('@hapi/joi');
 const { getValidatorError } = require('../helpers/validator');
 
+const rules = {
+    first_name: Joi.string().min(3).required(),
+    last_name: Joi.string().min(3).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z-0-9]{8,30}$')).required(),
+    password_confirmation: Joi.string().valid(Joi.ref('password')).required(),
+};
+
+const options = { abortEarly: false };
+
+const authSignIn = (request, response, next) => {
+    const { email, password } = request.body;
+
+    const schema = Joi.object({
+        email: rules.email,
+        password: rules.password,
+    });
+
+    const { error } = schema.validate({ email, password }, options);
+
+    if (error) {
+        const messages = getValidatorError(error, 'auth.signin');
+        return response.jsonBadRequest(null, null, { error: messages });
+    }
+
+    next();
+};
+
 const authSignUp = (request, response, next) => {
     const { first_name, last_name, email, password, password_confirmation } = request.body;
 
     const schema = Joi.object({
-        first_name: Joi.string().min(3).required(),
-        last_name: Joi.string().min(3).required(),
-        email: Joi.string().email().required(),
-        password: Joi.string().pattern(new RegExp('^[a-zA-Z-0-9]{8,30}$')).required(),
-        password_confirmation: Joi.string().valid(Joi.ref('password')).required(),
+        first_name: rules.first_name,
+        last_name: rules.last_name,
+        email: rules.email,
+        password: rules.password,
+        password_confirmation: rules.password_confirmation,
     });
 
-    const options = { abortEarly: false };
     const { error } = schema.validate(
-        { 
-            first_name, 
-            last_name, 
-            email, 
-            password, 
-            password_confirmation 
+        {
+            first_name,
+            last_name,
+            email,
+            password,
+            password_confirmation
         }, options);
 
     if (error) {
@@ -30,4 +57,4 @@ const authSignUp = (request, response, next) => {
     next();
 };
 
-module.exports = { authSignUp };
+module.exports = { authSignUp, authSignIn };
