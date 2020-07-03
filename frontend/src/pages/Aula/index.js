d557750caeb526d81ddc87d619cd0dc30b9732f5
Laurent Herman <31941580+LaurentHP@users.noreply.github.com>
2020-07-03 16:12:02 -0300

    Menu att. Rotas
    Atualização das rotas do menu lateral

--- frontend/src/pages/Aula/index.js
+++ frontend/src/pages/Aula/index.js
@@ -16,13 +16,13 @@ import EmojiEventsIcon from '@material-ui/icons/EmojiEvents';
 import PlaylistAddCheckIcon from '@material-ui/icons/PlaylistAddCheck';
 
 const items = [
-  { name: 'home', label: 'Home', icone: <HomeIcon style={{ color: 'white'}}/>, link: '#'  },
-  { name: 'iniciarAula', label: 'Iniciar aula', icone: <ScreenShareIcon style={{ color: 'white'}}/>, link: '#' },
-  { name: 'turmas', label: 'Turmas', icone: <SchoolIcon style={{ color: 'white'}}/>, link: '#' },
+  { name: 'home', label: 'Home', icone: <HomeIcon style={{ color: 'white'}}/>, link: '/dashboard'  },
+  { name: 'iniciarAula', label: 'Iniciar aula', icone: <ScreenShareIcon style={{ color: 'white'}}/>, link: '/aula' },
+  { name: 'turmas', label: 'Turmas', icone: <SchoolIcon style={{ color: 'white'}}/>, link: '/disciplinas' },
   { name: 'agendarAula', label: 'Agendar aulas', icone: <InsertInvitationIcon style={{ color: 'white'}}/>, link: '#' },
   { name: 'quizz', label: 'Quizz', icone: <EmojiEventsIcon style={{ color: 'white'}}/>, link: '#' },
-  { name: 'exercicios', label: 'Exercícios', icone: <PlaylistAddCheckIcon  style={{ color: 'white'}}/>, link: '#' },
-  { name: 'provas', label: 'Provas', icone: <PlaylistAddCheckIcon  style={{ color: 'white'}}/>, link: '#' },
+  { name: 'exercicios', label: 'Exercícios', icone: <PlaylistAddCheckIcon  style={{ color: 'white'}}/>, link: '/exercicios' },
+  { name: 'provas', label: 'Provas', icone: <PlaylistAddCheckIcon  style={{ color: 'white'}}/>, link: '/provas' },
 ]
 
 export default class aula extends Component {
