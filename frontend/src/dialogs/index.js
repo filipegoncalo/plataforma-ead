5b8d825163a0c2b4cfd514d7203e5c4b36c87445
william8Cabral <william8cabral@hotmail.com>
2020-07-10 17:52:10 -0300

    correcao

--- frontend/src/dialogs/index.js
+++ frontend/src/dialogs/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Button,Dialog,DialogActions,DialogContent,DialogContentText,DialogTitle,TextField} from '@material-ui/core';
-  import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     root: {
